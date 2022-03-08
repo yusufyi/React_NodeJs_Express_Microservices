3c6f777131a6d97ec99f271defbe3f5ddc015647
yusufyi <yusufyi@hotmail.com>
2022-03-08 14:37:31 +0000

    fixed comments

--- comments/index.js
+++ comments/index.js
@@ -1,28 +1,31 @@
 const express = require('express')
-const bosyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const commentsByPostId = {}
-app.get('/post/:id/commnets',(reg,res) => {
+app.get('/posts/:id/comments',(reg,res) => {
     res.send(commentsByPostId[reg.params.id] || [])
 
 })
 
-app.post('/post/:id/commnets',(reg,res) => {
+app.post('/posts/:id/comments',(req,res) => {
     const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body ;
-    const comments = commentsByPostId[reg.params.id] || [];
-    comments.push({id:commentId,content})
-    commentsByPostId [reg.params.id]  = comments;
-    res.status(201).send(comments);
+  const { content } = req.body;
 
+  const comments = commentsByPostId[req.params.id] || [];
 
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
     
 })
 
-app.listenerCount(4001, () => {
+app.listen(4001, () => {
     console.log('Listening on 4001');
 })
+
