552f3e7ab704fe415b64318900d0bff090da0cf9
yusufyi <yusufyi@hotmail.com>
2022-03-08 21:41:49 +0000

    React Create Post

--- post/index.js
+++ post/index.js
@@ -1,27 +1,31 @@
-
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const  { randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts',(reg,res) => {
-    res.send(posts);
-});
-//sd
-
-app.post('/posts',(reg,res) =>{
-    const id = randomBytes(4).toString('hex');
-    const {title } = reg.body;
-
-    posts[id] = {id,title};
-    res.status(201).send(posts[id]);
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.listen(4000,() => {
-    console.log('Listening on 4000');
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title
+  };
+
+  res.status(201).send(posts[id]);
 });
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
+
